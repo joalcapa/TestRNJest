6631c3b616e4d92c5cfe65c91629262d636248bf
jira2@feedback.com.ve <jira2@feedback.com.ve>
2018-08-16 11:51:49 -0400

    Home test modified

--- __tests__/Home-test.js
+++ __tests__/Home-test.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../src/components/Home';
+import { Provider } from 'react-redux';
+import ConfigureStore from '../src/store/ConfigureStore';
 
+let store = ConfigureStore();
 const expect = global.expect;
 
 describe("Component", () => {
     test('renders component Home', () => {
-        const tree = renderer.create(<Home />).toJSON();
+        const tree = renderer.create(
+            <Provider store={store}>
+                <Home />
+            </Provider>
+        ).toJSON();
         expect(tree).toMatchSnapshot();
     });
-
+/*
     test("functions of the Home component", () => {
-        let appComp = renderer.create(<Home />).getInstance();
+        let appComp = renderer.create(
+            <Provider store={store}>
+                <Home />
+            </Provider>
+        ).getInstance();
         appComp.change(7);
         //console.log("appComponent: ", appComp);
         expect(appComp.state.data).toEqual(70);
     });
+*/
 });
